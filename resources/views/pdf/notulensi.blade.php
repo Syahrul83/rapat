640bfee7cbe4d0effa27a5149d108c5088825b5f
syahrul anwar <valefour_syahrul@yahoo.com>
2026-06-19 22:56:39 +0800

    finish

--- resources/views/pdf/notulensi.blade.php
+++ resources/views/pdf/notulensi.blade.php
@@ -1,87 +1,205 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
-    <title>Notulensi - {{ $notulensi->meeting->title }}</title>
+    <title>Notulensi</title>
+
     <style>
-        body { font-family: 'DejaVu Sans', sans-serif; font-size: 12px; }
-        .header { text-align: center; margin-bottom: 30px; }
-        .header h2 { margin-bottom: 4px; }
-        .header .sub { font-size: 11px; color: #666; }
-        .info-table { width: 100%; border-collapse: collapse; margin-bottom: 20px; }
-        .info-table td { padding: 4px 8px; vertical-align: top; }
-        .info-table .label { width: 130px; font-weight: bold; }
-        .content { margin-bottom: 30px; line-height: 1.6; text-align: justify; }
-        .photos { margin-bottom: 30px; }
-        .photos img { max-height: 200px; max-width: 100%; margin: 8px; }
-        .signature-table { width: 100%; border: none; margin-top: 40px; }
-        .signature-table td { border: none; vertical-align: top; width: 50%; }
-        .signature-box { text-align: center; }
-        .signature-box .ttd-img { height: 50px; margin-bottom: 4px; }
-        .signature-box .name { font-weight: bold; margin-top: 4px; }
-        .signature-box .title { font-size: 11px; color: #666; }
-        .signature-box .line { border-bottom: 1px solid #000; width: 200px; margin: 4px auto; }
+        body {
+            font-family: DejaVu Sans, sans-serif;
+            font-size: 11pt;
+            margin: 20px;
+        }
+
+        .header {
+            width: 100%;
+            border-bottom: 2px solid #000;
+            padding-bottom: 10px;
+            margin-bottom: 15px;
+        }
+
+        .header-table {
+            width: 100%;
+        }
+
+        .logo {
+            width: 130px;
+            text-align: center;
+        }
+
+        .logo img {
+            width: 120px;
+            height: auto;
+        }
+
+        .instansi {
+            text-align: center;
+        }
+
+        .instansi h3,
+        .instansi h4,
+        .instansi p {
+            margin: 0;
+        }
+
+        .judul {
+            text-align: center;
+            font-size: 16px;
+            font-weight: bold;
+            margin: 15px 0;
+        }
+
+        .identitas {
+            width: 100%;
+            margin-bottom: 15px;
+        }
+
+        .identitas td {
+            padding: 3px;
+            vertical-align: top;
+        }
+
+        .isi {
+            min-height: 150px;
+            text-align: justify;
+            line-height: 1.5;
+        }
+
+        .foto {
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        .foto img {
+            width: 300px;
+            height: auto;
+        }
+
+        .ttd {
+            width: 100%;
+            margin-top: 40px;
+        }
+
+        .ttd td {
+            width: 50%;
+            text-align: center;
+            vertical-align: top;
+        }
+
+        .spasi-ttd {
+            height: 80px;
+        }
+
+        .alamat {
+            font-size: 11px;
+            margin: 0;
+        }
     </style>
 </head>
+
 <body>
+
     <div class="header">
-        <h2>NOTULENSI RAPAT</h2>
-        <p class="sub">Meeting Digital Service</p>
+        <table class="header-table">
+            <tr>
+                <td class="logo">
+                    <img src="{{ public_path('image/lambang.png') }}">
+                </td>
+
+                <td class="instansi">
+                    <h3>BADAN KARANTINA INDONESIA</h3>
+                    <h4>BALAI BESAR KARANTINA HEWAN IKAN DAN TUMBUHAN</h4>
+                    <h4>KALIMANTAN TIMUR</h4>
+
+                    <p class="alamat">Jl. Pelita No. 3 Sepinggan Balikpapan 76115</p>
+                    <p class="alamat">TELEPON : (0542) 8523292 FAKSIMILI : (0542) 413650</p>
+                    <p class="alamat">email : karantinakaltim@karantinaindonesia.go.id</p>
+                </td>
+            </tr>
+        </table>
     </div>
 
-    <table class="info-table">
+    <div class="judul">
+        NOTULENSI
+    </div>
+
+    <table class="identitas">
         <tr>
-            <td class="label">Nama Kegiatan</td>
-            <td>: {{ $notulensi->meeting->title }}</td>
+            <td width="180">Nama Kegiatan</td>
+            <td width="10">:</td>
+            <td>{{ $notulensi->meeting->title }}</td>
         </tr>
+
         <tr>
-            <td class="label">Tanggal Kegiatan</td>
-            <td>: {{ $notulensi->meeting_date->format('d M Y') }}</td>
+            <td>Tanggal Kegiatan</td>
+            <td>:</td>
+            <td>{{ $notulensi->meeting_date->format('d M Y') }}</td>
         </tr>
+
         <tr>
-            <td class="label">Lokasi</td>
-            <td>: {{ $notulensi->location }}</td>
+            <td>Lokasi Kegiatan</td>
+            <td>:</td>
+            <td>{{ $notulensi->location }}</td>
+        </tr>
+
+        <tr>
+            <td>Isi Notulensi </td>
+            <td>:</td>
+            <td> {{ $notulensi->isi_notulensi }} </td>
         </tr>
     </table>
 
-    <h3>Isi Notulensi</h3>
-    <div class="content">
-        {{ $notulensi->isi_notulensi }}
-    </div>
+    <div style="text-align: center; font-weight: bold;"> Foto Dokumentasi </div><br>
+    <br>
+    @if ($notulensi->photos->count() > 0)
 
-    @if($notulensi->photos->count() > 0)
-        <h3>Dokumentasi</h3>
-        <div class="photos">
-            @foreach($notulensi->photos as $photo)
+        <div class="foto">
+            @foreach ($notulensi->photos as $photo)
                 <img src="{{ public_path('storage/' . $photo->photo_path) }}" alt="Foto">
             @endforeach
         </div>
     @endif
 
-    <table class="signature-table">
+    <table class="ttd">
         <tr>
-            <td class="signature-box">
-                <p>Notulen</p>
-                <br>
-                @if($notulensi->notulen->ttd_image)
-                    <img src="{{ public_path('storage/' . $notulensi->notulen->ttd_image) }}" alt="TTD Notulen" class="ttd-img">
+            <td>
+                Notulen
+                @if ($notulensi->notulen->ttd_image == null)
+                    <div class="spasi-ttd"></div>
                 @endif
-                <div class="line">&nbsp;</div>
-                <p class="name">{{ $notulensi->notulen->name }}</p>
-                <p class="title">NIP. {{ $notulensi->notulen->nip }}</p>
+                @if ($notulensi->notulen->ttd_image)
+                    <br>
+                    <img width="150" src="{{ public_path('storage/' . $notulensi->notulen->ttd_image) }}"
+                        alt="TTD Notulen" class="ttd-img">
+                    <br>
+                @endif
+
+                <strong>{{ $notulensi->notulen->name }}</strong><br>
+                NIP. {{ $notulensi->notulen->nip }}
             </td>
-            <td class="signature-box">
-                <p>Mengetahui,</p>
-                <p>{{ $notulensi->meeting_date->format('d M Y') }}</p>
-                <br>
-                @if($notulensi->kepala->ttd_image)
-                    <img src="{{ public_path('storage/' . $notulensi->kepala->ttd_image) }}" alt="TTD Kepala" class="ttd-img">
+
+            <td>
+                Kepala Balai Besar Karantina<br>
+                Hewan, Ikan dan Tumbuhan<br>
+                Kalimantan Timur
+                @if ($notulensi->kepala->ttd_image == null)
+                    <div class="spasi-ttd"></div>
                 @endif
-                <div class="line">&nbsp;</div>
-                <p class="name">{{ $notulensi->kepala->name }}</p>
-                <p class="title">NIP. {{ $notulensi->kepala->nip }}</p>
+
+
+                @if ($notulensi->kepala->ttd_image)
+                    <br>
+                    <img width="150" src="{{ public_path('storage/' . $notulensi->kepala->ttd_image) }}"
+                        alt="TTD Kepala" class="ttd-img">
+                    <br>
+                @endif
+                <strong>{{ $notulensi->kepala->name }}</strong><br>
+                NIP. {{ $notulensi->kepala->nip }}
             </td>
         </tr>
     </table>
+
 </body>
+
 </html>
